2e22607f90c286bf4451f3c5ceb65f56877b7ae5
arishavelle18 <59736975+arishavelle18@users.noreply.github.com>
2021-09-05 20:02:32 +0800

    Update CaesarsCipher.js

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -1,13 +1,13 @@
 function rot13(str) {
-    let letterA = 'A'.charCodeAt();
-    let newArr=str.split("");
-    return newArr.map(element => {
-        if(/[A-Z]/.test(element)){
-             return /[A-Z]/.test(String.fromCharCode(element.charCodeAt()+13))
-                      ?String.fromCharCode(element.charCodeAt()+13) 
-                      :String.fromCharCode((element.charCodeAt()%13)+letterA);
+    let letterA = 'A'.charCodeAt();     // convert A into ascii value
+    let newArr=str.split("");           // split the str
+    return newArr.map(element => {      // map all the element of the newArr
+        if(/[A-Z]/.test(element)){      // use regex and test if the element  is compose of Uppercase characters
+             return /[A-Z]/.test(String.fromCharCode(element.charCodeAt()+13))   // check if the element in ascii value is in range of alphabetic value if you plus 13 
+                      ?String.fromCharCode(element.charCodeAt()+13)             // if true then it return ascii value +13 and then change it to character using String.fromCharCode
+                      :String.fromCharCode((element.charCodeAt()%13)+letterA);  // if false take the modulo of 13 and add to letterA which is 65
         }
-            return element;  
-    }).join("");
+            return element;  //return non-uppercase letter
+    }).join("");        // change to string 
   }
   console.log(rot13("SERR PBQR PNZC"));

41981a16603c759b821a79eb7275a2601d3f4447
arishavelle18 <59736975+arishavelle18@users.noreply.github.com>
2021-09-05 22:21:16 +0800

    Update PalindromeChecker.js

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -1,10 +1,10 @@
 function palindrome(str) {
-    function checkstring(str){
+    function checkstring(str){ // check if the str must only have number and letter
         return str.filter(item=>/[a-z0-9]/i.test(item));
     }
     
-    let newArray = str.split("").reverse();
-    return checkstring(newArray).join("").toUpperCase() === checkstring(str.split("")).join("").toUpperCase();
+    let newArray = str.split("").reverse();  // str must be split and reverse
+    return checkstring(newArray).join("").toUpperCase() === checkstring(str.split("")).join("").toUpperCase();  // this will check if you reverse the str then compare to original
 }
   
 console.log(palindrome("eyes_"));

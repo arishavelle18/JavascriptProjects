62e49d25c0239ed7fc5a72eaa8bdcd4f07613bfe
arishavelle18 <59736975+arishavelle18@users.noreply.github.com>
2021-09-05 22:28:34 +0800

    Update RomanNumeralConverter.js

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -1,5 +1,5 @@
-function convertToRoman(num) {
-    let romanNumeral={
+function convertToRoman(num) {   // recursion
+    let romanNumeral={ // declare a decending value of roman numeral
         'M':1000,
         'CM':900,
         'D':500,
@@ -14,13 +14,13 @@ function convertToRoman(num) {
         'IV':4, 
         'I':1                                  
     }
-    if(num===0){
+    if(num===0){        // return ""
         return "";
     }
     else{
-        for(let prop in romanNumeral){
-            if(num>=romanNumeral[prop]){
-                return prop + convertToRoman(num -romanNumeral[prop]);
+        for(let prop in romanNumeral){ // loop the num
+            if(num>=romanNumeral[prop]){    // if num is greater than roman numeral meaning it can duplicate the same letter in roman numeral
+                return prop + convertToRoman(num -romanNumeral[prop]); //concatenate each roman numeral
             }
         }
     }
